566ffce60f08beb38b8ec1b77f122ace9370d36b
Djoch25 <lorenzogioco94@libero.it>
2026-09-13 19:48:52 +0200

    Update globals.js

--- globals.js
+++ globals.js
@@ -7,7 +7,8 @@ const SCORE_FONT_SIZE = WIDTH * 0.05;
 const RELATIVE_NOTE_SPACING = WIDTH * 0.08;
 
 const TILE_SIZE = WIDTH / 12;
+const TILE_Y_OFFSET = 0.5;
 
 const LIFE_BAR_DIM = {x: null, y: null, w: null, h: null};
 
-const TOTAL_TIME = 50000;
+const TOTAL_TIME = 60000;
