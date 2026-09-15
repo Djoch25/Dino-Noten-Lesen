dd60c556d90964b6ffc349ea8fbf1f33c0a5d678
Djoch25 <lorenzogioco94@libero.it>
2026-09-15 14:22:24 +0200

    Update dino.js

--- dino.js
+++ dino.js
@@ -10,12 +10,12 @@ class BasicDino {
 		this.ctx = this.cnv.getContext("2d");
 		this.cnv.style.pointerEvents = "none";
 
-		this.x = x;
+		this.x = x - DINO_IMAGE_WIDTH * SCALE;
 		this.initX = this.x;
-		this.y = y;
-		this.groundY = this.y + DINO_IMAGE_ELEVATION; //posizione originale prima del salto
-		this.w = DINO_IMAGE_WIDTH * dim * SCALE;
-		this.h = DINO_IMAGE_HEIGHT * dim * SCALE;
+		this.y = y - DINO_IMAGE_HEIGHT * SCALE;
+		this.groundY = this.y + DINO_IMAGE_ELEVATION * SCALE; //posizione originale prima del salto
+		this.w = DINO_IMAGE_WIDTH * dim;
+		this.h = DINO_IMAGE_HEIGHT * dim;
 
 		this.dinoImgs = dinoImgs;
 		this.imgI = 0;
